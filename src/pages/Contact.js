35c02e79bc8f18a4a13d8fffa9a57d6ae6335c92
Ethan Frakes <edfrakes@gmail.com>
2025-08-26 22:13:41 -0400

    Update email address, fix header link formatting on mobile, add Orbitron font for non-text box content

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,7 +6,7 @@ export default function Contact() {
   return (
     <main className="site-content">
       <h1>Contact</h1>
-      <p><MdOutlineEmail/><a href="mailto:edfrakes@icloud.com">Email</a></p>
+      <p><MdOutlineEmail/><a href="mailto:ethan@ethanfrakes.com">Email</a></p>
       <p><SiLinkedin/><a href="https://www.linkedin.com/in/ethan-frakes-b03070157/">LinkedIn</a></p>
       <p><SiGithub/><a href="https://github.com/thefrostedfrakes">GitHub</a></p>
     </main>
