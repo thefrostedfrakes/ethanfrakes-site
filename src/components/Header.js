6ff6f2b4fd3e438b88c1da45b2e0b3bddcd04181
Ethan Frakes <edfrakes@gmail.com>
2025-09-09 21:29:05 -0400

    Move page links to top of page, wrap in solid black box

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,15 +5,15 @@ export default function Header() {
     return (
         <header className="site-header">
             {/* <img src={logo} className="site-logo" alt="logo" /> */}
-            <h1>Ethan Frakes</h1>
-            <p><small>Welcome to my website!</small></p>
-            <nav>
+            <nav className="site-nav">
                 <Link to="/">Home</Link> | {' '}
                 <Link to="/about">About</Link> | {' '}
                 <Link to="/portfolio">Portfolio</Link> | {' '}
                 <Link to="/publications">Publications</Link> | {' '}
                 <Link to="/contact">Contact</Link> | {' '}
             </nav>
+            <h1>Ethan Frakes</h1>
+            <p><small>Welcome to my website!</small></p>
         </header>
     );
 }
